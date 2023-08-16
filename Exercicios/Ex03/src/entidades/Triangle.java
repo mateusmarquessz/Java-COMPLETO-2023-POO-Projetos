e542aa58567e00887083c87623bf7b49a1889366
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-08-16 18:21:18 -0300

    att

--- Exercicios/Ex03/src/entidades/Triangle.java
+++ Exercicios/Ex03/src/entidades/Triangle.java
@@ -1,12 +1,16 @@
-package entidades;
+package entidades; //Pacote da classe
 
-public class Triangle {
-    public double a;
-    public double b;
-    public double c;
+public class Triangle { //nome da classe
+    //Prefixo public indica que o atributo ou metodo pode ser usado em outros arquivos
+    public double a; //Atributos da classe
+    public double b; //Atributos da classe
+    public double c; //Atributos da classe
 
+    //Double = Tipo de dados que o motodo retorna(se o metodo nao retorna nada, usa-se a palavra "void"
+    //Area = Nome do metodo
+    //() = Lista de parametros do metodo
     public double area(){
         double p = (a + b + c) /2.0;
         return Math.sqrt(p *(p - a) * (p - b) * (p - c));
-    }
+    }  //Corpo do metodo
 }
