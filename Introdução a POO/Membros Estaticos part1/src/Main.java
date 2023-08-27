fb5bdb2fafa2e743fc071db7cea845b4eccef773
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-08-27 14:56:48 -0300

    att

---
+++ Introdução a POO/Membros Estaticos part1/src/Main.java
@@ -0,0 +1,10 @@
+//Membros = atributos e metodos
+/*
+Membros estaticos
+
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
