419af43f46b4fc8cd22fbfd1465d17dcb6e515e2
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-08-27 21:22:23 -0300

    att

---
+++ Construtores, palavra this, sobrecarga, encapsulamento/Encapsulamento/src/Main.java
@@ -0,0 +1,36 @@
+import Dados.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data:");
+        System.out.printf("Name: ");
+        String nome = sc.nextLine();
+        System.out.printf("Price");
+        Double preco = sc.nextDouble();
+        System.out.printf("Quantity in stock:");
+        int quantity = sc.nextInt();
+        Product product = new Product(nome, preco, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.AddProducts(quantity);
+
+        System.out.println("Update data: " + product);
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.RemoveProducts(quantity);
+
+        System.out.println("Update data: " + product);
+        sc.close();
+    }
+}
---
+++ Construtores, palavra this, sobrecarga, encapsulamento/Sobrecarga/src/Dados/Product.java
@@ -0,0 +1,35 @@
+package Dados;
+
+public class Product {
+    public String nome;
+    public double preco;
+    public int quantity;
+
+    public Product(String nome, double preco, int quantity) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantity = quantity;
+    }
+
+    public Product(String nome, double preco) {
+        this.nome = nome; ////SobreCarga e disponibilizar mais uma versao da mesma operacao com a diferenca nos parametros
+        this.preco = preco;
+    }
+
+    public double totalValueInStock(){
+        return preco *quantity;
+    }
+
+    public void AddProducts(int quantity){
+        this.quantity += quantity; //acessando o atributo por causa do this
+    }
+
+    public void RemoveProducts(int quantity){
+        this.quantity -= quantity; //
+    }
+
+    public String toString() {
+        return nome + ", $ " + String.format("%,2f", preco) + ", " + quantity + "units, Total: $ " + String.format("%,2f", totalValueInStock());
+    }
+}
+
